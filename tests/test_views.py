054a9541677bcab80fd3a351f02709e1840d36a5
Zhuoyun Wei <wzyboy@wzyboy.org>
2023-07-08 18:26:55 -0700

    test: DIY

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,27 +1,24 @@
-class TestViews:
-    def test_index(self, client):
-        resp = client.get('/tweet/')
-        print(resp.text)
-        assert 'Twitter Archive' in resp.text
-        assert 'keyboard' in resp.text
+class TestMediaReplacement:
+    tweet_id = '1615425412921987074'
+    media_filename = 'Fmsk2gHacAAJGL0.jpg'
+    cf_domain = 'd1111111111.cloudfront.net'
 
     def test_direct_media(self, client):
         client.application.config['T_MEDIA_FROM'] = 'direct'
-        resp = client.get('/tweet/1615425412921987074.html')
-        assert 'https://pbs.twimg.com/media/Fmsk2gHacAAJGL0.jpg' in resp.text
+        resp = client.get(f'/tweet/{self.tweet_id}.html')
+        assert f'https://pbs.twimg.com/media/{self.media_filename}' in resp.text
 
     def test_mirror_media(self, client):
-        CF_DOMAIN = 'd1111111111.cloudfront.net'
         client.application.config['T_MEDIA_FROM'] = 'mirror'
         client.application.config['T_MEDIA_MIRRORS'] = {
-            'pbs.twimg.com': f'{CF_DOMAIN}/pbs.twimg.com',
-            'video.twimg.com': f'{CF_DOMAIN}/video.twimg.com',
+            'pbs.twimg.com': f'{self.cf_domain}/pbs.twimg.com',
+            'video.twimg.com': f'{self.cf_domain}/video.twimg.com',
         }
-        resp = client.get('/tweet/1615425412921987074.html')
-        assert f'https://{CF_DOMAIN}/pbs.twimg.com/media/Fmsk2gHacAAJGL0.jpg' in resp.text
+        resp = client.get(f'/tweet/{self.tweet_id}.html')
+        assert f'https://{self.cf_domain}/pbs.twimg.com/media/{self.media_filename}' in resp.text
 
     def test_fs_media(self, client):
         client.application.config['T_MEDIA_FROM'] = 'filesystem'
         client.application.config['T_MEDIA_FS_PATH'] = './media'
-        resp = client.get('/tweet/1615425412921987074.html')
-        assert '/tweet/media/pbs.twimg.com/media/Fmsk2gHacAAJGL0.jpg' in resp.text
+        resp = client.get(f'/tweet/{self.tweet_id}.html')
+        assert f'/tweet/media/pbs.twimg.com/media/{self.media_filename}' in resp.text
